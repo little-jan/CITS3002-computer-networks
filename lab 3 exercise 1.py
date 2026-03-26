ec614cb79d2152c1a288ea11ed04ac549c3f6cc4
iroha <78338683+little-jan@users.noreply.github.com>
2026-03-26 10:27:49 +0800

    hamming distance simple code

---
+++ lab 3 exercise 1.py
@@ -0,0 +1,26 @@
+def valid_binary(value):
+    if len(value) != 8:
+        return False
+    else:
+        value = str(value)
+        for num in range(len(str(value))):
+            if value[num] != "0" and value[num] != "1":
+                return False
+        else:
+            return True
+
+def hamming_distance(bin1, bin2):
+    if not valid_binary(bin1) or not valid_binary(bin2):
+        print("Invalid input")
+        return
+    else:
+        hamming = []
+        for i in range(8):
+            if bin1[i] != bin2[i]:
+                hamming.append("1")
+            else:
+                hamming.append("0")
+        new = "".join(hamming)
+        final = int(new)
+        return final
+print(hamming_distance("10101010", "01010110"))
